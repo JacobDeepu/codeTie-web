2da683e9df8f70b992b13cadd88e2977dcf6ea38
Arjun P S <arjunvayalar77@gmail.com>
2025-03-05 16:20:21 +0530

    vite cofig changes deleted

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://codetie.onrender.com", // Change this to your backend URL
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
